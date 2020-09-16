7a9f9d2565af55859da6acb6efdf80ccd2d19a49
surajit <surajit@poweredbypeople.io>
2020-09-17 02:16:35 +0300

    minor fix

--- logger/setup.py
+++ logger/setup.py
@@ -6,4 +6,5 @@ setup(
     scripts=[
         "MQloggerpub"
     ],  # The name of your scipt, and also the command you'll be using for calling it
+    download_url='https://github.com/surajit003/rabbitmq-python-logger-pub/archive/v1.0.tar.gz'
 )
